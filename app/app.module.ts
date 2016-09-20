a60eb6cc9510b89ff56668398aad66043f300122
ganeshbabu <ganeshbabu.atech@gmail.com>
2016-09-19 20:45:55 -0500

    routing, spearation of concern, styles

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
 
 @NgModule({
-	imports: [ BrowserModule, FormsModule ],
-	declarations: [ AppComponent, HeroDetailComponent ],
+	imports: [ BrowserModule, FormsModule, routing ],
+	declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+	providers: [ HeroService ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
